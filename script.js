0f65bf653ed5a5b4e6de49b696681e3dc21fe7c9
Dtruk <kiendt273@gmail.com>
2023-07-26 05:12:32 +0700

    Bootstrap

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ form.addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent the default form submission behavior
   window.location.href = 'home.html';
 });
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all buttons with class "button"
+  const buttons = document.querySelectorAll(".button");
+
+  // Add click event listeners to each button
+  buttons.forEach(button => {
+      button.addEventListener("click", function() {
+          // Get the data-target value from the button
+          const targetId = button.getAttribute("data-target");
+
+          // Get the div with the corresponding ID
+          const targetDiv = document.getElementById(targetId);
+
+          // Toggle the visibility of the div
+          targetDiv.classList.toggle("show");
+      });
+  });
+});
